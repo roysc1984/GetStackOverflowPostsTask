5beb2e4627f7417d1eb58a1f512fca4f4bef4b88
roys <roys@supplant.me>
2021-02-16 13:11:57 +0200

    remove useEffect in userDetails

--- src/Components/UserDetails.tsx
+++ src/Components/UserDetails.tsx
@@ -1,25 +1,19 @@
 import { observer } from 'mobx-react'
-import React, { FC, useEffect, useState } from 'react'
+import React, { FC } from 'react'
 import { StyleSheet, View, Image } from 'react-native'
-import { Owner } from '../Stores/AppStore'
 import { useStores } from '../Stores/StoreContext'
 import TextMode from './TextMode'
 
 const UserDetails: FC= () => {
   const { appStore } = useStores()
-  const [owner, setOwner] = useState<Owner | undefined>(undefined)
-  
-  useEffect(() => {
-    setOwner(appStore.getOwnerAvatar)
-  }, [appStore.postQuestions])
-
+ 
   return (
     <View style={styles.container}>
-      {owner?.profile_image && <Image resizeMode='contain' style={styles.avatar} source={{uri: owner?.profile_image}} />}
+      {appStore.getOwnerAvatar?.profile_image && <Image resizeMode='contain' style={styles.avatar} source={{uri: appStore.getOwnerAvatar?.profile_image}} />}
       <View style={styles.containerInfo}>
-        <TextMode>{`Name: ${owner?.display_name || ''}`}</TextMode>
-        <TextMode>{`Reputation: ${owner?.reputation || ''}`}</TextMode>
-        <TextMode>{`Accept Rate:  ${owner?.accept_rate || ''}`}</TextMode>
+        <TextMode>{`Name: ${appStore.getOwnerAvatar?.display_name || ''}`}</TextMode>
+        <TextMode>{`Reputation: ${appStore.getOwnerAvatar?.reputation || ''}`}</TextMode>
+        <TextMode>{`Accept Rate:  ${appStore.getOwnerAvatar?.accept_rate || ''}`}</TextMode>
       </View>
     </View>
   )
